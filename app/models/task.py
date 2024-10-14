28a40b4c028e5bcfc73920f789cee94c4d3e8008
alexwlad3885 <alexwlad3885@yandex.ru>
2024-10-14 20:56:38 +0300

    Домашнее задание по теме "Модели SQLALchemy. Отношения между таблицами."

---
+++ app/models/task.py
@@ -0,0 +1,24 @@
+from app.baskend.db import *
+from app.models import *
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+
+class Task(Base):
+
+    __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    slug = Column(String, unique=True, index=True)
+
+    user = relationship('User', back_populates='tasks')
+
+
+print(CreateTable(Task.__tablename__))
