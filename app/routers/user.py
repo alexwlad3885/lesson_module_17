c6440dc0d527cf4c329a8917b83fa50279717690
alexwlad3885 <alexwlad3885@yandex.ru>
2024-10-19 12:50:13 +0300

    Домашнее задание по теме "Использование БД в маршрутизации. Часть 2."

--- app/routers/user.py
+++ app/routers/user.py
@@ -25,6 +25,16 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
     return user_by
 
 
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user_by = db.scalar(select(User).where(User.id == user_id))
+    if user_by is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
+    tasks_by_user = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+
+    return tasks_by_user
+
+
 @router.post('/create')
 async def create_user(db: Annotated[Session, Depends(get_db)], create_user: CreateUser):
     db.execute(insert(User).values(username=create_user.username,
@@ -40,10 +50,8 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user: Crea
 async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, update_user: UpdateUser):
     user_update = db.scalar(select(User).where(User.id == user_id))
     if user_update is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User was not found"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
+
     db.execute(update(User).where(User.id == user_id).values(firstname=update_user.firstname,
                                                              lastname=update_user.lastname,
                                                              age=update_user.age))
@@ -57,5 +65,12 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
     if user_update is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
     db.execute(delete(User).where(User.id == user_id))
+
+    tasks_by_user = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    if tasks_by_user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task was not found")
+
+    db.execute(delete(Task).where(Task.user_id == user_id))
+
     db.commit()
     return {'status_code': status.HTTP_200_OK, 'transaction': 'User has been deleted'}
